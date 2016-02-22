14fb8a761c7595eadb91b4b351845b71d01d324d
mpthornbrugh <mpthornbrugh@gmail.com>
2016-02-22 17:18:15 -0600

    Added initial feel of website

--- main/main.js
+++ main/main.js
@@ -11,6 +11,56 @@ angular.module('portfolio.main', ['ngRoute'])
 
 	.controller('MainCtrl', ['$scope', '$location', function ($scope, $location) {
 
+		var MIN_WIDTH_OF_TILES = 300;
+		var MIN_HEIGHT_OF_TILES = 200;
+
+		$scope.tiles = [
+			{
+				"description":"Logo",
+				"color":"red"
+			},
+			{
+				"description":"About Me",
+				"color":"blue"
+			},
+			{
+				"description":"Social",
+				"color":"green"
+			},
+			{
+				"description":"Welcome Message",
+				"color":"pink"
+			},
+			{
+				"description":"Projects",
+				"color":"blue"
+			},
+			{
+				"description":"Contact Me",
+				"color":"green"
+			},
+			{
+				"description":"Picture",
+				"color":"yellow"
+			},
+			{
+				"description":"Resume",
+				"color":"beige"
+			}
+		];
+
+		var calculatedWidth = ($(window).width() - 58)/2;
+		var calculatedHeight = MIN_HEIGHT_OF_TILES;
+
+		$("<style>.tile { width: " + calculatedWidth + "px; height: " + calculatedHeight + "px; float: left; }</style>").appendTo('body');
+
+		var wrapper = $("#wrapper");
+		wrapper.height(($scope.tiles.length/2) * calculatedHeight);
+
+		window.onresize = function () {
+			wrapper.height(($scope.tiles.length/2) * calculatedHeight);
+		};
+
 	}]);
 
 
